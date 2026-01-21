566cf58760f60a9126c957ae9b6b3364e46ed595
Alex Roth <alex@magmastone.net>
2026-01-21 08:32:32 +0800

    Add CANFD/MCAN peripheral definitions (#32)
    * add canfd peripherals
    
    * document where transforms were derived
    
    * remove empty comment
    
    * fix source comment
    
    * canfd: arrify and other cleanups
    
    * canfd: add canfd to perimap
    
    * Modify upstream PR #15 to rename CAN register blocks
    
    This allows access to the meaningful registers and is enough
    to get the peripheral working to send and receive frames.
    
    I have confirmed that interrupts also work as expected.
    
    This produces some slightly awkward to use definitions due to the
    presence of the TI_WRAPPER grouping. This is something that I think
    needs to be addressed in chiptool, but per some discussion in PR #15 and
    in Matrix, this is at least a working place to start and we can improve
    from here.
    
    ---------
    
    Co-authored-by: Liam Kinne <liam@kinne.io>
    Co-authored-by: Iooon <jonas17mueller@gmail.com>
    Co-authored-by: Alex Roth <alex@alexroth.me>

--- mspm0-data-gen/src/perimap.rs
+++ mspm0-data-gen/src/perimap.rs
@@ -13,6 +13,7 @@ pub static PERIMAP: RegexMap<&str> = RegexMap::new(&[
     (".*:adc", "v1"),
     (".*:wwdt", "v1"),
     (".*:trng", "v1"),
+    (".*:canfd", "v1"),
     ("mspm0c110x:sysctl", "c110x"),
     ("mspm0c1105_c1106:sysctl", "c110x"),
     ("msps003fx:sysctl", "c110x"),
